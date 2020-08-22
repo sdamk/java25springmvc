86617062fa9f7c4cedde87efd2e6a403b698353d
MichalK <michal.kotlinski92@gmail.com>
2020-08-22 13:24:41 +0200

    Layout i18n

--- src/main/java/pl/sda/springmvc/springmvc/SpringmvcApplication.java
+++ src/main/java/pl/sda/springmvc/springmvc/SpringmvcApplication.java
@@ -1,7 +1,10 @@
 package pl.sda.springmvc.springmvc;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 public class SpringmvcApplication {
